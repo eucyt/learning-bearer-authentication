7f411fb70f036bb0c3a7ed0b1d9e3398dd67678a
yuta <eypc.ofun@gmail.com>
2022-08-18 17:39:32 +0900

    remove unused files and table

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,12 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import {Module} from '@nestjs/common';
+import {AuthModule} from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
 })
-export class AppModule {}
+export class AppModule {
+}
